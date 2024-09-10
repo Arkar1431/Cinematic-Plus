cf587a0777f42953c1b59fe042b0e216fba7aece
MeassavrinSavuth <u6520177@au.edu>
2024-09-10 13:20:55 +0700

    Initial Commit

---
+++ CinematicPlus/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CinematicPlus
+//
+//  Created by Mac on 9/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
